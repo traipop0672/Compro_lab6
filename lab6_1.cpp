5896b77d709163983fc5e80c7b36125ea0002b95
Traipop <traipopwichiansarn@gmail.com>
2025-12-09 16:21:47 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int num ,odd =0 , even =0 ;
+    while(true){
+        cout << "Enter an integer: ";
+        cin >> num ;
+        if(num == 0){
+            break;
+        } else {
+            if(num%2 == 0){
+                even++;
+            } else {
+                odd++;
+            }
+        }
+    }
+    cout << "#Even numbers = " << even <<"\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
